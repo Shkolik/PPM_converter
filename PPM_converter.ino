7c6b35c368a76883110a8b0326853a1d971888ae
Andrew Shkolik <andrew.shkolik@selerix.com>
2018-08-29 05:09:20 +0300

    - channel order fixed - data synchronization fixed

--- PPM_converter.ino
+++ PPM_converter.ino
@@ -3,17 +3,17 @@
 
 //////////////////////CONFIGURATION///////////////////////////////
 #define default_servo_value 1500  //set the default servo value
-#define min_servo_value 1000  //set the min servo value
-#define max_servo_value 2000  //set the max servo value
+#define min_servo_value 950  //set the min servo value
+#define max_servo_value 2050  //set the max servo value
 
 #define PPM_FrGap 3000  //set the gap between PPM frames microseconds (1ms = 1000µs)
 #define PPM_FrLen 20500  //set the PPM frame length in microseconds (1ms = 1000µs)
 #define PPM_PulseLen 300  //set the pulse length
 #define onState 0  //set polarity of the pulses: 1 is positive, 0 is negative
-#define outPin 5 //set PPM signal output pin on the arduino
+#define outPin 1 //set PPM signal output pin on the arduino
 #define interruptPin 10 //set digital pin to listen PPM //Atmega16
 //#define interruptPin 2 //set digital pin to listen PPM //Arduino
-#define channelAmount 6 //set number of channels, 8 channels max
+const byte channelAmount = 6; //set number of channels, 8 channels max
 
 //#define TIMSK1 TIMSK // override timer1 register for Atmega16
 
@@ -23,8 +23,8 @@ const unsigned int chanelsOutput[] = {2,0,1,3,4,5,6,7};
 
 const boolean debug = false; //set true if you want see output in serial port
 //////////////////////////////////////////////////////////////////
-volatile unsigned int input[channelAmount];
-unsigned int output[channelAmount];
+volatile unsigned long input[channelAmount];
+volatile unsigned long output[channelAmount];
 
 //PPMReader ppm(interruptPin, channelAmount);
 
@@ -34,12 +34,13 @@ void setup() {
   for(int i = 0; i < channelAmount; i++)
   {
     output[i] = default_servo_value;
+    input[i] = default_servo_value;
   }
 
   pinMode(interruptPin, INPUT_PULLUP);
   pinMode(outPin, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(interruptPin), READER_ISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), reader_ISR, FALLING);
   
   if(debug){
     Serial.begin(9600);
@@ -62,10 +63,11 @@ void setup() {
 void loop() {
     
   // Get latest valid values from all channels
-  for (int channel = 0; channel < channelAmount; ++channel) {
-    unsigned int value = input[channel];
+  for (int channel = 0; channel < channelAmount; channel++) {
+    unsigned long value = input[chanelsOutput[channel]];
 
-    output[chanelsOutput[channel]] = value;
+    output[channel] = value;
+    //output[chanelsOutput[channel]] = value;
     
     if(debug)
     {
@@ -78,33 +80,35 @@ void loop() {
   }
 }
 
-READER_ISR()
+void reader_ISR()
 {
-  static byte cur_chan_numb;
-  static unsigned long microsAtLastPulse;
+  static byte channel;
+  static unsigned long microsAtLastPulse = 0;
   
     // Remember the current micros() and calculate the time since the last pulseReceived()
     unsigned long previousMicros = microsAtLastPulse;
     microsAtLastPulse = micros();
-    unsigned int pulseTime = microsAtLastPulse - previousMicros;
+    unsigned long pulseTime = microsAtLastPulse - previousMicros;
 
-    if (pulseTime >= PPM_FrGap) {
+    if (pulseTime >= PPM_FrGap || channel >= channelAmount) {
         /* If the time between pulses was long enough to be considered an end
          * of a PPM frame, prepare to read channel values from the next pulses */
-        cur_chan_numb = 0;
+        channel = 0;
     }
     else {
         // Store times between pulses as channel values
-        if (cur_chan_numb < channelAmount) {
+        if (channel < channelAmount) {
             
-            if (pulseTime >= min_servo_value - 10 && time <= max_servo_value + 10) {
-                validValues[pulseCounter] = constrain(pulseTime, min_servo_value, max_servo_value);
+            if (pulseTime >= min_servo_value - 10 && pulseTime <= max_servo_value + 10) {
+                input[channel] = constrain(pulseTime, min_servo_value, max_servo_value);
             }
             else{
-              input[cur_chan_numb] = default_servo_value;
+              input[channel] = default_servo_value;
               }
+              ++channel;
         }
-        ++cur_chan_numb;
+        
+        
     }
 }
 
@@ -124,7 +128,7 @@ ISR(TIMER1_COMPA_vect){
   }
   else{  //end pulse and calculate when to start the next pulse
     static byte cur_chan_numb;
-    static unsigned int calc_rest;
+    static unsigned long calc_rest;
   
     digitalWrite(outPin, !onState);
     state = true;
