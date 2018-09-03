894773f8285633f4d865904c761d57018ba04036
Andrew Shkolik <andrew.shkolik@selerix.com>
2018-09-04 01:18:47 +0300

    - Timer2-based PPMwriter (osciloscop-tested)

--- test/sketch_sep03a/sketch_sep03a.ino
+++ test/sketch_sep03a/sketch_sep03a.ino
@@ -1,13 +1,15 @@
-#define default_servo_value 1500  //set the default servo value
-#define min_servo_value 950  //set the min servo value
-#define max_servo_value 2050  //set the max servo value
+#define default_servo_value 3000//1500  //set the default servo value
+#define min_servo_value 2000//950  //set the min servo value
+#define max_servo_value 4000//2050  //set the max servo value
 
-#define PPM_FrGap 3000  //set the gap between PPM frames microseconds (1ms = 1000µs)
-#define PPM_FrLen 20500  //set the PPM frame length in microseconds (1ms = 1000µs)
-#define PPM_PulseLen 300  //set the pulse length
+#define PPM_FrGap 6000//3000  //set the gap between PPM frames microseconds (1ms = 1000µs)
+#define PPM_FrLen 41000//20500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PulseLen 600//300  //set the pulse length
 #define onState 0  //set polarity of the pulses: 1 is positive, 0 is negative
 #define OCR2A_full 249;
-#define outPin 1 //set PPM signal output pin
+#define outPin 5 //set PPM signal output pin
+
+const bool debug = false;
 
 const byte channelAmount = 6; //set number of channels, 8 channels max
 //Chanels order recieving AETR1234
@@ -22,61 +24,111 @@ volatile unsigned int channel;
 volatile unsigned long input[channelAmount];
 volatile unsigned long output[channelAmount];
 
-volatile unsigned int interruptCount;
-volatile unsigned int OCR2A_rest;
-
-
 ISR(TIMER2_COMPA_vect)
 {
+  static unsigned int interruptCount = 0;
+  static unsigned int OCR2A_rest = 0;
+  static unsigned int OCR2A_next = OCR2A_full;
   static boolean state = true;
-  static byte channel;
+  static boolean wait = false;
+  static byte channel = 0;
   static unsigned long calc_rest;
+
+
   TCNT2 = 0; //reset counter
 
   if (interruptCount > 0)
   {
     interruptCount--;
   }
+
+  //start pulse
+  if (state)
+  {
+    if (interruptCount < 1)
+    {
+      if (!wait)
+      {
+        digitalWrite(outPin, onState);
+        interruptCount = 1 + PPM_PulseLen / 250;
+        wait = true;
+      }
+      else
+      {
+        OCR2A_next = OCR2A_rest;
+        state = false;
+        wait = false;
+      }
+    }
+    else
+    {
+      OCR2A_next = OCR2A_full;
+      OCR2A_rest = PPM_PulseLen % 250;
+    }
+  }
   else
   {
-    //start pulse
-    if (state) {
-      digitalWrite(outPin, onState);
-      interruptCount = 1;
-      OCR2A = PPM_PulseLen - 250;
-      state = false;
-    }
-    else {
-      //end pulse and calculate when to start the next pulse
-      digitalWrite(outPin, !onState);
-      state = true;
-
-      if (channel >= channelAmount) 
+    //Serial.println(String(channel));
+    //end pulse and calculate when to start the next pulse
+    if (channel >= channelAmount)
+    {
+      unsigned int channelLength = (PPM_FrLen - calc_rest - PPM_PulseLen);
+      if (interruptCount < 1)
       {
-        unsigned int channelLength = (PPM_FrLen - calc_rest - PPM_PulseLen);
-        interruptCount = channelLength / 250;
-        if(interruptCount > 1)
+        if (!wait)
         {
-          OCR2A = OCR2A_full;
-          OCR2A_rest = channelLength % 250;
+          interruptCount  = channelLength / 250;
+          digitalWrite(outPin, !onState);
+          wait = true;
         }
         else
         {
-          }
-        calc_rest = 0;
-        channel = 0;
+          OCR2A_next = OCR2A_rest;
+          state = true;
+          wait = false;
+          calc_rest = 0;
+          channel = 0;
+        }
       }
-      else {
-        unsigned long value = output[channel];
-        unsigned int channelLength = value - PPM_PulseLen;
-        interruptCount = channelLength / 250;
+      else
+      {
+        OCR2A_next = OCR2A_full;
         OCR2A_rest = channelLength % 250;
-        calc_rest += value;
-        channel++;
+      }
+
+    }
+    else
+    {
+      unsigned long value = output[channel];
+      unsigned int channelLength = value - PPM_PulseLen;
+      if (interruptCount < 1)
+      {
+        if (!wait)
+        {
+          interruptCount = channelLength / 250;
+          digitalWrite(outPin, !onState);
+          wait = true;
+        }
+        else
+        {
+          OCR2A_next = OCR2A_rest;
+          calc_rest += value;
+          channel++;
+          state = true;
+          wait = false;
+        }
+      }
+      else
+      {
+        OCR2A_next = OCR2A_full;
+        OCR2A_rest = channelLength % 250;
       }
     }
   }
+
+  OCR2A = OCR2A_next;
 }
+
 
 
 ISR (TIMER1_OVF_vect)
@@ -101,7 +153,7 @@ ISR (TIMER1_CAPT_vect)
   currentTimeStamp = (overflowCopy << 16) + timer1CounterValue;
 
   unsigned long value = currentTimeStamp - prevTimeStamp;
-  if (channel >= 6 || value >= 6000)
+  if (channel >= channelAmount || value >= PPM_FrGap)
   {
     channel = 0;
   }
@@ -118,8 +170,9 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(outPin, OUTPUT);
+  pinMode(10, OUTPUT);
   digitalWrite(outPin, !onState);  //set the PPM signal pin to the default state (off)
-
+  digitalWrite(10, 0);
   noInterrupts ();  // protected code
   //disable Timer0
   TCCR0A = 0;
@@ -139,21 +192,32 @@ void setup() {
 
   //setup timer2
   //Setup Timer2 to fire every 0.5us
-  TCCR2 = 0; // set entire TCCR1 register to 0
+  TCCR2A = 0; // set entire TCCR1 register to 0
+  TCCR2B = 0;
   TCNT2 = 0;
 
   TCCR2A = bit (WGM21); // CTC
   TCCR2B = bit(CS21);  // 8 prescaler: 0,5 microseconds at 16mhz, 1 microsecond at 8mhz (Atmega16L)
-  OCR2A = 249;         // count to 250 (zero-relative)
+  OCR2A = OCR2A_full;         // count to 250 (zero-relative)
 
   TIMSK2 |= bit (OCIE2A);
   interrupts ();
 }
 
 void loop() {
-  for (int channel = 0; channel < 6; channel++)
-  {
-    Serial.print(String(input[channel]) + " ");
+  // Get latest valid values from all channels
+  for (byte channel = 0; channel < channelAmount; channel++) {
+    unsigned long value = input[chanelsOutput[channel]];
+
+    output[channel] = constrain(value, min_servo_value, max_servo_value);
+
+    if (debug)
+    {
+      Serial.print(String(value) + " ");
+    }
   }
-  Serial.println();
+  if (debug)
+  {
+    Serial.println();
+  }
 }
